e931874399bd0d65a82bf01a449491d249392f9a
Filippos <filippossiokouros@gmail.com>
2020-05-22 11:52:08 +0100

    Added testing libraries and cleaned code

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,14 @@
 import React from 'react';
-import { render } from '@testing-library/react';
 import App from './App';
+import ReactDOM from 'react-dom';
+import './setupTests';
+// import { shallow } from 'enzyme';
+// import renderer from 'react-test-renderer';
 
-test('renders learn react link', () => {
-	const { getByText } = render(<App />);
-	const linkElement = getByText(/learn react/i);
-	expect(linkElement).toBeInTheDocument();
+describe('App component', () => {
+	it('renders without crashing', () => {
+		const div = document.createElement('div');
+		ReactDOM.render(<App />, div);
+		ReactDOM.unmountComponentAtNode(div);
+	});
 });
