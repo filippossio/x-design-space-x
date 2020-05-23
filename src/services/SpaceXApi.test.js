fb2743d089fc459d4d1a6378f61c401a12d45626
Filippos <filippossiokouros@gmail.com>
2020-05-23 16:20:24 +0100

    Added tests for axios call

--- src/services/SpaceXApi.test.js
+++ src/services/SpaceXApi.test.js
@@ -1,7 +1,28 @@
 import { getLaunches } from './SpaceXApi';
+import axios from 'axios';
+import { data } from './SpaceXMockData';
+
+jest.mock('axios');
 
 describe('Space X api calls', () => {
 	it('Exists', () => {
 		expect(getLaunches).toBeDefined();
 	});
-});
+
+	it('GIVEN getLauches is called WHEN thr response is correct THEN ir should be eqaul to the data.', () => {
+		axios.get.mockResolvedValue(data);
+		return getLaunches()
+			.then(res => {
+				expect(res).toBeDefined();
+				expect(res).toEqual(data);
+			});
+	});
+
+	it('GIVEN getLauches is called WHEN thr response is incorrect THEN ir should be eqaul to the data.', () => {
+		axios.get.mockRejectedValue(data);
+		return getLaunches()
+			.catch(error => {
+				expect(error).toBeDefined();
+			});
+	});
+}); 
