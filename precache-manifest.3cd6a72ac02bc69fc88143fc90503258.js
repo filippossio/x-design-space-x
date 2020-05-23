014870f76c48a40e3e9a24a911869275246df09c
Filippos <filippossiokouros@gmail.com>
2020-05-23 16:55:33 +0100

    Updates

---
+++ precache-manifest.3cd6a72ac02bc69fc88143fc90503258.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f762abde3fad09fe107472a703a0f901",
+    "url": "/filippossio/x-design-space-x/index.html"
+  },
+  {
+    "revision": "94d5d3627d0f2c4e1ea3",
+    "url": "/filippossio/x-design-space-x/static/css/main.ae705a6e.chunk.css"
+  },
+  {
+    "revision": "227f4c1e46935e0f0d9a",
+    "url": "/filippossio/x-design-space-x/static/js/2.428a6416.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/filippossio/x-design-space-x/static/js/2.428a6416.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "94d5d3627d0f2c4e1ea3",
+    "url": "/filippossio/x-design-space-x/static/js/main.066f3a30.chunk.js"
+  },
+  {
+    "revision": "d1a01c1d9747e0bf46ce",
+    "url": "/filippossio/x-design-space-x/static/js/runtime-main.68497b32.js"
+  },
+  {
+    "revision": "1fa4147b0544c87a8933322999a887c5",
+    "url": "/filippossio/x-design-space-x/static/media/BrandonGrotesque-Black.1fa4147b.woff2"
+  },
+  {
+    "revision": "33bc2fdf69d61c0abd8f43d41c995e87",
+    "url": "/filippossio/x-design-space-x/static/media/BrandonGrotesque-Black.33bc2fdf.woff"
+  },
+  {
+    "revision": "3bebd53c0ee89703d8829e97e2ca0784",
+    "url": "/filippossio/x-design-space-x/static/media/BrandonGrotesque-Black.3bebd53c.ttf"
+  },
+  {
+    "revision": "0b59275f161f6f43f0ca3b2365a77f77",
+    "url": "/filippossio/x-design-space-x/static/media/BrandonGrotesque-Bold.0b59275f.ttf"
+  },
+  {
+    "revision": "1ad8d40bebcdcc1f79f0e22ce35b3d75",
+    "url": "/filippossio/x-design-space-x/static/media/BrandonGrotesque-Bold.1ad8d40b.woff"
+  },
+  {
+    "revision": "441b33aac91c0f5883e390134d6fa6eb",
+    "url": "/filippossio/x-design-space-x/static/media/BrandonGrotesque-Bold.441b33aa.woff2"
+  },
+  {
+    "revision": "08ee9db6dc65093bb66dbaae7094f087",
+    "url": "/filippossio/x-design-space-x/static/media/BrandonGrotesque-Medium.08ee9db6.woff2"
+  },
+  {
+    "revision": "920c11f315161dc7ff0204c5502105e2",
+    "url": "/filippossio/x-design-space-x/static/media/BrandonGrotesque-Medium.920c11f3.woff"
+  },
+  {
+    "revision": "ef4fddb0ac2f5cdf0b2eb54bf9898fb0",
+    "url": "/filippossio/x-design-space-x/static/media/BrandonGrotesque-Medium.ef4fddb0.ttf"
+  },
+  {
+    "revision": "013a89eb356fb88baad2f6178c5400ba",
+    "url": "/filippossio/x-design-space-x/static/media/BrandonGrotesque-Regular.013a89eb.woff"
+  },
+  {
+    "revision": "aba7d12553de107511905eb7595a72d9",
+    "url": "/filippossio/x-design-space-x/static/media/BrandonGrotesque-Regular.aba7d125.ttf"
+  },
+  {
+    "revision": "aff015dfe96d4c9021ace79558be225d",
+    "url": "/filippossio/x-design-space-x/static/media/BrandonGrotesque-Regular.aff015df.woff2"
+  },
+  {
+    "revision": "10cc8d8755a6ebdc69c8e4fa006de6fc",
+    "url": "/filippossio/x-design-space-x/static/media/BrandonGrotesque-RegularItalic.10cc8d87.woff"
+  },
+  {
+    "revision": "5267a22825e445ebb40686639b9c1cd8",
+    "url": "/filippossio/x-design-space-x/static/media/BrandonGrotesque-RegularItalic.5267a228.woff2"
+  },
+  {
+    "revision": "ba6abfddb0c1143b43d41ce9bc6fc236",
+    "url": "/filippossio/x-design-space-x/static/media/BrandonGrotesque-RegularItalic.ba6abfdd.ttf"
+  },
+  {
+    "revision": "950ba66541181838b6c07f14d62e331a",
+    "url": "/filippossio/x-design-space-x/static/media/spacex-logo.950ba665.png"
+  }
+]);
